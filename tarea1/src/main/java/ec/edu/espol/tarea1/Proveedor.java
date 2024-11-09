ac8e0bda3c97aac4ce97c2528e9a1a7d312dfde0
salva1984 <salvamusa2005@gmail.com>
2024-11-08 23:39:48 -0500

    resueltos errores

--- tarea1/src/main/java/ec/edu/espol/tarea1/Proveedor.java
+++ tarea1/src/main/java/ec/edu/espol/tarea1/Proveedor.java
@@ -1,11 +1,8 @@
+package ec.edu.espol.tarea1;
+public class Proveedor{
+
 private int idProveedor;
     private String nombre;
 
     public void registrarVehiculo() {}
-}
-
-public class Notificacion {
-    private int idNotificacion;
-
-    public void enviarNotificacion() {}
 }
