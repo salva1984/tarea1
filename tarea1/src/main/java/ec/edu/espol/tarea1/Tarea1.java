43f63a4643411e9b94d84edc934810a7ef2cebbc
PaulPer37 <paulisaacpo@hotmail.com>
2024-11-08 23:15:43 -0500

    crear tarea

---
+++ tarea1/src/main/java/ec/edu/espol/tarea1/Tarea1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.tarea1;
+
+/**
+ *
+ * @author RUCO HOUSE
+ */
+public class Tarea1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
