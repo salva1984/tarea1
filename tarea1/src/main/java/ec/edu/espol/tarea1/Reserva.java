91728cd497116377f635f63cd22783f39399e1de
PaulPer37 <paulisaacpo@hotmail.com>
2024-11-08 23:35:26 -0500

    a

---
+++ tarea1/src/main/java/ec/edu/espol/tarea1/Reserva.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.tarea1;
+
+import java.util.Date;
+
+/**
+ *
+ * @author RUCO HOUSE
+ */
+class Reserva {
+    private int idReserva;
+    private Pago pago;
+    private String destino;
+    private Date fecha;
+    private String preferencias;
+    private Vuelo vuelo;
+    private EstadoIncedente incidente;
+    private Vehiculo vehiculo;
+
+    public Reserva(int idReserva, Pago pago, String destino, Date fecha, String preferencias, Vuelo vuelo, EstadoIncedente incidente, Vehiculo vehiculo) {
+        this.idReserva = idReserva;
+        this.pago = pago;
+        this.destino = destino;
+        this.fecha = fecha;
+        this.preferencias = preferencias;
+        this.vuelo = vuelo;
+        this.incidente = incidente;
+        this.vehiculo = vehiculo;
+    }
+
+    
+    public void confirmarReserva() {
+    }
+
+    public void cancelarReserva() {
+    }
+
+    public void verificarDisponibilidad() {
+    }
+
+    public void reportarIncidente() {
+    }
+}
